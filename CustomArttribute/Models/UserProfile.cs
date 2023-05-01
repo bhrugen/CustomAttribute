2ad698e8a2be2bfd98091490f391b214e05d266a
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-04-30 21:14:26 -0500

    Small Update

--- CustomArttribute/Models/UserProfile.cs
+++ CustomArttribute/Models/UserProfile.cs
@@ -6,8 +6,6 @@ namespace CustomArttribute.Models
     public class UserProfile
     {
         public string Name { get; set; }
-        [Required]
-        [MaxLength]
         [MaxFileResolution(1)]
         [AllowedExtensions(new string[] { ".jpg", ".png" ,".jpeg"})]
         public IFormFile? Image { get; set; }
